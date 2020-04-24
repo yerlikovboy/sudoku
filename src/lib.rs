f7791c72029b722f2f374ee7442d7c747e10725d
Ali Guler <maguler@gmail.com>
2020-04-24 10:48:30 -0500

    refactor console game

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
 // in this file, all we basically do is make modules avaialable for everyone to use.
 pub mod types;
-pub use crate::types::board::Board;
 pub use crate::types::cell::Cell;
+pub use crate::types::puzzle::Puzzle;
