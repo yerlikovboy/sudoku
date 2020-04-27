f4a31424298de56a6ace568760881473ea2e8a91
Ali Guler <maguler@gmail.com>
2020-04-27 13:06:23 -0500

    user move is now a console module function

--- src/console/utils.rs
+++ src/console/utils.rs
@@ -1,6 +1,7 @@
 use std::fs::File;
 use std::io::Read;
 
+use sudoku::Cell;
 use sudoku::Puzzle;
 
 pub fn print_console(p: &Puzzle) {
@@ -38,4 +39,24 @@ pub fn from_file(file_name: &str) -> std::io::Result<Puzzle> {
 
     assert_eq!(grid.len(), 81);
     Ok(Puzzle::new(grid.as_slice()))
+}
+
+pub fn user_move(row: u8, column: u8, value: u8) -> Result<Cell, String> {
+    if row < 1 || row > 9 {
+        return Err(String::from("row must be in the range 1..9"));
+    }
+
+    if column < 1 || column > 9 {
+        return Err(String::from("column must be in the range 1..9"));
+    }
+    if value > 9 {
+        return Err(String::from("new value must be between 0 and 9"));
+    }
+
+    Ok(Cell {
+        row: row - 1,
+        column: column - 1,
+        value: if value == 0 { None } else { Some(value) },
+        previous_value: None,
+    })
 }
